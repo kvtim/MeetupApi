3d4b9104c4bb46ee664dfee0a2456ffb9b269775
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-02-14 16:37:09 +0300

    Add DbContext

---
+++ Meetup/Meetup.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Meetup.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             : base(options)
+        {
+        }
+    }
+}
