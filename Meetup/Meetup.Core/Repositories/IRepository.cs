53ccf85bb2aa010d416a05a887bbe5cc61da266d
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-02-14 16:18:14 +0300

    Add base generic repository

---
+++ Meetup/Meetup.Core/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.Core.Repositories
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
