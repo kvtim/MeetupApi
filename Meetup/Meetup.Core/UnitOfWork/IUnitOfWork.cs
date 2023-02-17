adf9fe627235024a852e85867e5cb0b88f251b12
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-02-17 17:33:58 +0300

    Add UnitOfWork

---
+++ Meetup/Meetup.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Meetup.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IMeetingRepository MeetingRepository { get; }
+        IParticipantRepository ParticipantRepository { get; }
+
+        void Commit();
+        void Rollback();
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
---
+++ Meetup/Meetup.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Meetup.Core.Repositories;
+using Meetup.Core.UnitOfWork;
+using Meetup.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IMeetingRepository _meetingRepository;
+        private IParticipantRepository _participantRepository;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IMeetingRepository MeetingRepository => _meetingRepository = 
+            _meetingRepository ?? new MeetingRepository(_context);
+
+        public IParticipantRepository ParticipantRepository => _participantRepository = 
+            _participantRepository ?? new ParticipantRepository(_context);
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Rollback()
+        {
+            _context.Dispose();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
