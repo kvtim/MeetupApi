3349d90cad7bc3be3d8f38f5d7cc8f76f64d645f
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-02-19 17:38:34 +0300

    Add base and empty controllers

---
+++ Meetup/Meetup.Api/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meetup.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
--- Meetup/Meetup.Api/Program.cs
+++ Meetup/Meetup.Api/Program.cs
@@ -1,6 +1,14 @@
+using Meetup.Domain.Repositories;
+using Meetup.Domain.UnitOfWork;
+using Meetup.Infascructure.Repositories;
+using Meetup.Infascructure.UnitOfWork;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
