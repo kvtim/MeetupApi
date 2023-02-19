6e95ba45bbeb2f6e6f28ebf18317dba63d50ca22
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-02-19 18:47:23 +0300

    Add automapper

---
+++ Meetup/Meetup.Api/Mappings/MeetingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Meetup.Domain.Models;
+using Meetup.Infrastructure.Dtos.Meeting;
+
+namespace Meetup.Api.Mappings
+{
+    public class MeetingProfile : Profile
+    {
+        public MeetingProfile()
+        {
+            CreateMap<Meeting, CreateMeetingDto>().ReverseMap();
+            CreateMap<Meeting, UpdateMeetingDto>().ReverseMap();
+            CreateMap<Meeting, MeetingDto>().ReverseMap();
+        }
+    }
+}
